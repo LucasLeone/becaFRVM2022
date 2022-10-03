04d3fc05bc28bf0c817711a9f48cdaf009c920c8
LucasLeone <lucasagustinleone2003@gmail.com>
2022-10-03 19:25:55 -0300

    db script added

---
+++ app/db/script.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS public.interesado
+(
+    id_interesado integer NOT NULL DEFAULT nextval('interesado_id_interesado_seq'::regclass),
+    nombre character varying(30) COLLATE pg_catalog."default",
+    apellido character varying(30) COLLATE pg_catalog."default",
+    telefono character varying(35) COLLATE pg_catalog."default",
+    email character varying(50) COLLATE pg_catalog."default",
+    direccion character varying(40) COLLATE pg_catalog."default",
+    numero integer,
+    dni character varying COLLATE pg_catalog."default",
+    localidad character varying COLLATE pg_catalog."default",
+    fecharegistro date,
+    curso_fk integer,
+    CONSTRAINT interesado_pkey PRIMARY KEY (id_interesado)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.interesado
+    OWNER to postgres;
+
+CREATE TABLE IF NOT EXISTS public.curso
+(
+    id_curso integer NOT NULL DEFAULT nextval('curso_id_curso_seq'::regclass),
+    nombre character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT curso_pkey PRIMARY KEY (id_curso)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.curso
+    OWNER to postgres;
